c792b785c8dee7447059c4f97e565539fa2bc8ff
dhthinh7 <dhthinh7@gmail.com>
2022-06-15 09:04:39 +0700

    Add vertical chart

---
+++ src/components/VerticalChart/VerticalChart.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+const options = {
+  elements: {
+    bar: {
+      borderWidth: 5,
+      hoverBackgroundColor: 'blue',
+    }
+  },
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'right',
+      display: false,
+      labels: {
+        boxWidth: 20,
+      }
+    },
+    title: {
+      display: true,
+      text: '2020',
+
+    },
+  },
+
+};
+
+const labels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const data = {
+  labels,
+  borderWidth: 3
+};
+
+export const VerticalChart = ({dataChart}) => {
+  return <div>
+    <Bar data={{
+      datasets: [{
+        data: labels.map((item, index)=>dataChart.netWorthGrowth[index]),
+        label:'Vertical',
+        borderWidth: 1,
+        Width: "20px"
+      }],
+      ...data,
+    }} options={{
+      ...options,
+    }}/>
+  </div>
+}
