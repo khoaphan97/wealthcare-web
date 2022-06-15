6e3ace28585249a655b7513877c0ab57c4c0c1fc
dhthinh7 <dhthinh7@gmail.com>
2022-06-15 09:04:39 +0700

    Add vertical chart

--- src/containers/Dashboard/Summary/Networth.js
+++ src/containers/Dashboard/Summary/Networth.js
@@ -3,39 +3,45 @@ import withContext from '../../../components/ContextTab'
 import ToolSprites from '../../../components/ToolSprites'
 import apiController from '../../../api'
 import { convertToDollarFormat } from '../../../utils/helper'
+import { VerticalChart } from '../../../components/VerticalChart/VerticalChart'
 
 const Networth = ({ openContext }) => {
-  const [datas, setData] = useState({ netWorth: "" });
-
+  const [datas, setData] = useState({data: {netWorth: "", chartData: ""}});
+  
   const handleOpenContext = () => {
     openContext(
       <div>Hellooo</div>,
       'Networth information'
-    )
-  }
-
-  useEffect(() => {
-    const getDashboardData = async () => {
-      const data = await apiController.getDashboardData();
-      setData({ netWorth: convertToDollarFormat(data.netWorth)});
+      )
     }
-    getDashboardData();
-  }, [])
+    
+    useEffect(() => {
+      const getDashboardData = async () => {
+        const data = await apiController.getDashboardData();
+        // setData({ netWorth: convertToDollarFormat(data.netWorth)});
+        setData({data: data})
+      }
+      getDashboardData();
+    }, [])
 
+    let {netWorth, chartData} = datas.data;
+    // console.log("thinh" , netWorth);
   return (
     <div className='nw middle-item'>
       <div className="nw-infor">
         <div className='txt-middle-title' onClick={handleOpenContext}>Networth</div>
         <div className="nw-income">
-          {datas.netWorth}
+          {convertToDollarFormat(netWorth)}
           <span><i className="fa fa-eye"></i></span>
         </div>
       </div>
       <div className="nw-chart">
-        chart
-      </div>
-      <div className="wtc-button">
-        <ToolSprites spriteName={"setting"} />
+        <div className="nw-chart-vertical">
+          <VerticalChart dataChart = {chartData}/>
+        </div>
+        <div className="wtc-button">
+          <ToolSprites spriteName={"setting"} />
+        </div>
       </div>
     </div>
   )
